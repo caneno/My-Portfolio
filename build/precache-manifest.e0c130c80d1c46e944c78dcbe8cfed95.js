ea3cbd85d4e5af521f3ece909e792aafc25d2403
Ray <ray.vas89@protonmail.com>
2019-11-06 14:54:21 -0800

    added build

---
+++ build/precache-manifest.e0c130c80d1c46e944c78dcbe8cfed95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4e31593963d4ba9ab3dbeca3742295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40d9222d1a2629dc023",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "8bb91cee05a819e51b47",
+    "url": "/static/css/main.154dbfae.chunk.css"
+  },
+  {
+    "revision": "f40d9222d1a2629dc023",
+    "url": "/static/js/2.4e3d3d52.chunk.js"
+  },
+  {
+    "revision": "8bb91cee05a819e51b47",
+    "url": "/static/js/main.bf231265.chunk.js"
+  },
+  {
+    "revision": "74ef57dcc864c3ae2fcc",
+    "url": "/static/js/runtime-main.a759db38.js"
+  }
+]);
