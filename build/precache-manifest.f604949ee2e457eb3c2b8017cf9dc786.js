34c6ba596391a86654aeaa0bcab6606234c98b67
Ray <ray.vas89@protonmail.com>
2019-11-06 23:44:46 -0800

    fixed bugs

---
+++ build/precache-manifest.f604949ee2e457eb3c2b8017cf9dc786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba9238d7b389cb614db6cffb2b9920b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f40d9222d1a2629dc023",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "b55e6a3057c3a895f487",
+    "url": "/static/css/main.154dbfae.chunk.css"
+  },
+  {
+    "revision": "f40d9222d1a2629dc023",
+    "url": "/static/js/2.4e3d3d52.chunk.js"
+  },
+  {
+    "revision": "b55e6a3057c3a895f487",
+    "url": "/static/js/main.460671fb.chunk.js"
+  },
+  {
+    "revision": "74ef57dcc864c3ae2fcc",
+    "url": "/static/js/runtime-main.a759db38.js"
+  }
+]);
